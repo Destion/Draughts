0b686d5d6f6d335160682ad35e94191ad5f7f091
Destion <mick.sneekes@live.nl>
2015-10-13 14:48:43 +0200

    Created main class with GUI-test.

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(600, 600);
+        frame.setTitle("Jay, it works!");
+        frame.setResizable(false);
+        frame.setVisible(true);
+    }
+
+}
