be41b464f97220c27c2f01a3fc68052cfffb913e
Destion <mick.sneekes@live.nl>
2015-10-30 12:02:55 +0100

    Testing the comcont

--- src/controllers/CommunicationController.java
+++ src/controllers/CommunicationController.java
@@ -59,9 +59,8 @@ public class CommunicationController {
 
         for (int i=0; i<10; i++){
             for (int j=0; j<15; j+=3){
-                //int temp = bytes.get(0);
+                int temp = bytes.get(0);
 
-                int temp = 101;
                 int bit1 = temp / 100;
                 int bit2 = (temp % 100) / 10;
                 int bit3 = temp % 10;
