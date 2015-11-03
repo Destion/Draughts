ca56c6d42d39cb4893b9fc67a766c6de6882b0eb
Destion <mick.sneekes@live.nl>
2015-11-03 14:23:24 +0100

    fixed issues with bits

--- src/controllers/CommunicationController.java
+++ src/controllers/CommunicationController.java
@@ -111,7 +111,7 @@ public class CommunicationController {
             }
             gpio.high(pins.get(15));
             try {
-                Thread.sleep(10);
+                Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
